c530b59e9232a46e4a5528a99075718ec0e203a3
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-06-20 09:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function removeArticle(name) {
+  return name.replace(/^(a|an|the)\s+/i, '');
+}
+
+function sortBandNames(names) {
+  return names.sort((a, b) => {
+    const nameA = removeArticle(a.toLowerCase());
+    const nameB = removeArticle(b.toLowerCase());
+    return nameA.localeCompare(nameB);
+  });
+}
+
+const sortedBandNames = sortBandNames(bandNames);
+
+const bandList = document.getElementById('band');
+
+sortedBandNames.forEach((name) => {
+  const li = document.createElement('li');
+  li.textContent = name;
+  bandList.appendChild(li);
+});
